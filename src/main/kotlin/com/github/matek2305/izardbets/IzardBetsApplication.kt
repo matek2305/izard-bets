451648c34c0889120db9f8f3255c5302e643ae8a
Mateusz Urbański <matek2305@gmail.com>
2017-10-16 18:48:55 +0200

    project template

---
+++ src/main/kotlin/com/github/matek2305/izardbets/IzardBetsApplication.kt
@@ -0,0 +1,11 @@
+package com.github.matek2305.izardbets
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class IzardBetsApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(IzardBetsApplication::class.java, *args)
+}
