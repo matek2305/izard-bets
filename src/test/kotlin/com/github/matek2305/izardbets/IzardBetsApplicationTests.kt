6f1568d1169b71acc36ba3b1db63fbf786897028
Mateusz Urbański <matek2305@gmail.com>
2017-11-01 18:26:32 +0100

    added ktlint to gradle build

--- src/test/kotlin/com/github/matek2305/izardbets/IzardBetsApplicationTests.kt
+++ src/test/kotlin/com/github/matek2305/izardbets/IzardBetsApplicationTests.kt
@@ -9,8 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner
 @SpringBootTest
 class IzardBetsApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
